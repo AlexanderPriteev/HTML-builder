0b188d5f3ef6a8a8eee33e765962791f92d78cba
Alexander Priteev <priteev@sibers.com>
2022-05-22 17:17:46 +0600

    feat: 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+
+const buildFile = path.join(__dirname, './project-dist/bundle.css');
+const directoryPath = path.join(__dirname, './styles');
+
+fs.writeFile(buildFile, '', (error) =>{
+  if (error) throw error;
+});
+
+fs.readdir(directoryPath, (error, files) => {
+  if (error) throw error;
+  
+  files.forEach((e) => {
+    const thisFile = path.join(directoryPath, e);
+    const fileType = path.extname(thisFile);
+
+
+    fs.stat(thisFile, (error, stats) => {
+      if (error) throw error;
+
+      if (stats.isFile() && fileType === '.css') {
+        const stream = new fs.ReadStream(thisFile, {encoding: 'utf-8'});
+        stream.on('readable', () => {
+          const data = stream.read();
+          if (data != null) {
+            fs.appendFile(buildFile, `${data}\r\n`, (error) =>{
+              if (error) throw error;
+            });
+          }
+        });
+      }
+
+    });
+  });
+});
